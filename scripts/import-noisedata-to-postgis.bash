e657b8cc556479c956ffff97d5dd1ed390809237
Markus Koskinen <markus.koskinen@iki.fi>
2019-10-22 22:51:40 +0300

    improvements to the noisedata import script: import to tmp table, remove older than 1y data, remove coord duplicates, except the newest entry. etc.

--- scripts/import-noisedata-to-postgis.bash
+++ scripts/import-noisedata-to-postgis.bash
@@ -1,11 +1,29 @@
 #!/bin/bash
 #
 # This is a quick proof of concept. Please read it thorugh before running.
+# Please also conserve the noisedata bandwidth as well as you can.
 #
 # Author: Markus Koskinen
+# License: BSD
 #
 # Enable PostGIS in pg and export postgres credentials in format as follows:
 # POSTGRES_CREDENTIALS="host=noisedata.postgis-example.com port=5432 user='postgres' password='secret' dbname='noisedata'"
+
+# remove duplicates and over 1y old entries from the tmp_table then copy data to the main table
+function tmp_table_cleanup_and_copy()
+{
+  echo "Deleting over 1y old data from temporary table ..."
+  psql $POSTGRES_URI -c "DELETE FROM tmp_noisedata WHERE time_iso8601 < (current_date - interval '12 months');"
+  echo "Deleting non-latest duplicate coordinate entries from temporary table ..."
+  psql $POSTGRES_URI -c "DELETE FROM tmp_noisedata tmp1 using tmp_noisedata tmp2 WHERE tmp1.time_iso8601 < tmp2.time_iso8601 AND tmp1.wkb_geometry = tmp2.wkb_geometry;"
+  echo "Initial noisedata table create. This not elegant and will error often."
+  psql $POSTGRES_URI -c "CREATE TABLE IF NOT EXISTS noisedata AS SELECT * FROM tmp_noisedata;"
+  echo "Copy tmp_noisedata to noisedata ..."
+  psql $POSTGRES_URI -c "INSERT INTO noisedata(time_iso8601,time_epoch,time_gps_iso8601,time_gps_epoch,noise_level,accuracy,wkb_geometry) SELECT time_iso8601,time_epoch,time_gps_iso8601,time_gps_epoch,noise_level,accuracy,wkb_geometry FROM tmp_noisedata;"
+  echo "Dropping tmp table and vacuuming ..."
+  psql $POSTGRES_URI -c "DROP TABLE tmp_noisedata;"
+  psql $POSTGRES_URI -c "VACUUM;"
+}
 
 # export all the points.geojson files in workdir to PostGIS
 function points_gis_export_loop()
@@ -18,38 +36,56 @@ function points_gis_export_loop()
     echo "Handling pointsfile: ${pointsfile}"
 
     # Insert geojsons to PostGIS   
-    ogr2ogr -f PostgreSQL PG:"${POSTGRES_CREDENTIALS}" "${pointsfile}" -nln noisedata
+    ogr2ogr -f PostgreSQL PG:"${POSTGRES_CREDENTIALS}" "${pointsfile}" -nln tmp_noisedata
+    tmp_table_cleanup_and_copy
     rm "${pointsfile}"
   done
   IFS=$SAVEIFS
 }
 
-# Clean up old directories
+# Handle the zip files in ./data.noise-planet.org/noisecapture/, extract one zip at a time to ./workdir/
+function zip_extraction_loop()
+{
+  SAVEIFS=$IFS
+  IFS=$(echo -en "\n\b")
+
+  for zfile in $(ls -1 ./data.noise-planet.org/noisecapture/*.zip)
+  do
+    echo "Handling zipfile: ${zfile}"
+    unzip "${zfile}" "*points*" -d ./workdir/
+
+    # Insert
+    if [ $? -eq 0 ]; then
+      points_gis_export_loop
+    fi
+    rm "${zfile}"
+  done
+
+  IFS=$SAVEIFS
+}
+
+# Check for ogr2ogr
+which ogr2ogr > /dev/null
+if [ $? -ne 0 ]; then
+  echo "Error: Missing ogr2ogr binary, please install it first."
+fi
+
+# Clean up old directories, drop old database.
+echo "Clean up working directories ..."
 rm -rf ./workdir/
 mkdir ./workdir
+echo "Dropping old noisedata ..."
+psql $POSTGRES_URI -c "DROP TABLE IF EXISTS noisedata;"
+psql $POSTGRES_URI -c "DROP TABLE IF EXISTS tmp_noisedata;"
+
+echo "Initialize main table ..."
+# Import a small file to init the main table (assuming run location)
+ogr2ogr -f PostgreSQL PG:"${POSTGRES_CREDENTIALS}" data/Algeria_Oran_Arzew.points.geojson -nln noisedata
 
 # Get the zipfiles
 rm -rf ./data.noise-planet.org
-wget -r -np -R "index.html*" https://data.noise-planet.org/noisecapture/
+# -N newer only, -c continue interrupted - check this works
+wget -c -N -r -np -R "index.html*" https://data.noise-planet.org/noisecapture/
 
-# They will endup in a subdir ./data.noise-planet.org/noisecapture/
-
-# From there lets extract one zip at a time to ./workdir/
-
-SAVEIFS=$IFS
-IFS=$(echo -en "\n\b")
-
-for zfile in $(ls -1 ./data.noise-planet.org/noisecapture/*.zip)
-do
-  echo "Handling zipfile: ${zfile}"
-  unzip "${zfile}" "*points*" -d ./workdir/
-
-  # Insert
-  if [ $? -eq 0 ]; then
-    points_gis_export_loop
-  fi
-  rm "${zfile}"
-done
-
-IFS=$SAVEIFS
+zip_extraction_loop
 
